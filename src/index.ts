b1bb25e22b946f826156a84148a60133743bfc6f
Mex Delgado <mdelgado@tresdoce.com.ar>
2022-03-18 10:49:01 -0300

    chore(apim-1234): edit index file
    Lorem ipsum sarasa sarasa
    
    Ref: https://github.com/rudemex/nestjs-test-pkg

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
-// export public api from here
-// for example:
-// export * from './decorators';
-// export { Decorators } from './decorators';
 export { GreetingModule } from './greeting/greeting.module';
 export { GreetingService } from './greeting/services/greeting.service';
